5fb7e8c669c70333dd4a7e6e83f9d7559dc4f9ab
Albert Wang <albert@cellabus.com>
2015-04-23 19:37:10 -0700

    Finish and add tests for AppleDEPClient::Request

--- lib/apple_dep_client/request.rb
+++ lib/apple_dep_client/request.rb
@@ -10,8 +10,15 @@ module AppleDEPClient
       if headers == nil
         headers = make_headers
       end
-      # TODO
-
+      request = Typhoeus::Request.new(
+        url,
+        method: query_type,
+        body: body,
+        headers: headers
+      )
+      request.run
+      check_response request.response
+      request.response.body
     end
 
     def self.make_headers
@@ -22,6 +29,20 @@ module AppleDEPClient
         'X-ADM-Auth-Session' => session_auth_token,
         'Content-Type' => 'application/json;charset=UTF8',
       }
+    end
+
+    def self.check_response response
+      if response.code == 400 and response.body.strip == 'MALFORMED_REQUEST_BODY'
+        raise AppleDEPClient::Error::MalformedRequest.new response.body
+      elsif response.code == 401 and response.body.strip == 'UNAUTHORIZED'
+        raise AppleDEPClient::Error::Unauthorized.new response.body
+      elsif response.code == 403 and response.body.strip == 'FORBIDDEN'
+        raise AppleDEPClient::Error::Forbidden.new response.body
+      elsif response.code == 405
+        raise AppleDEPClient::Error::MethodNotAllowed.new response.body
+      elsif response.code != 200
+        raise AppleDEPClient::Error::RequestError.new response.body
+      end
     end
   end
 end
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe AppleDEPClient::Request do
+  before(:each) do
+    Typhoeus::Expectation.clear
+  end
   describe ".make_request" do
     it "can make a request and return the body" do
-
+      expect(AppleDEPClient::Request).to receive(:make_headers).once
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 200, body: 'qwer')
+      Typhoeus.stub('url').and_return response
+      expect(AppleDEPClient::Request).to receive(:check_response).with(response).once
+      body = AppleDEPClient::Request.make_request('url', 'get', 'asdf')
+      expect(body).to eq 'qwer'
+    end
+    it "can override default headers" do
+      expect(AppleDEPClient::Request).to_not receive(:make_headers)
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 200, body: 'qwer')
+      Typhoeus.stub('url').and_return response
+      expect(AppleDEPClient::Request).to receive(:check_response).with(response).once
+      body = AppleDEPClient::Request.make_request('url', 'get', 'asdf', {})
+      expect(body).to eq 'qwer'
     end
   end
   describe ".make_headers" do
@@ -14,6 +30,32 @@ describe AppleDEPClient::Request do
       expect(headers['X-Server-Protocol-Version']).to eq '2'
       expect(headers['X-ADM-Auth-Session']).to eq 'asdf'
       expect(headers['Content-Type']).to eq 'application/json;charset=UTF8'
+    end
+  end
+  describe ".check_response" do
+    it "will not raise an error if the response is ok" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 200)
+      expect{AppleDEPClient::Request.check_response response}.to_not raise_error
+    end
+    it "can raise a MalformedRequest error" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' MALFORMED_REQUEST_BODY ')
+      expect{AppleDEPClient::Request.check_response response}.to raise_error AppleDEPClient::Error::MalformedRequest
+    end
+    it "can raise a Unauthorized error" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 401, body: 'UNAUTHORIZED')
+      expect{AppleDEPClient::Request.check_response response}.to raise_error AppleDEPClient::Error::Unauthorized
+    end
+    it "can raise a Forbidden error" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 403, body: 'FORBIDDEN')
+      expect{AppleDEPClient::Request.check_response response}.to raise_error AppleDEPClient::Error::Forbidden
+    end
+    it "can raise a MethodNotAllowed error" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 405)
+      expect{AppleDEPClient::Request.check_response response}.to raise_error AppleDEPClient::Error::MethodNotAllowed
+    end
+    it "can raise an error" do
+      response = Typhoeus::Response.new(return_code: :ok, response_code: 500)
+      expect{AppleDEPClient::Request.check_response response}.to raise_error AppleDEPClient::Error::RequestError
     end
   end
 end
