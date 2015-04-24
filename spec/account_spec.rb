d54aea2a7f36896552ab0dce9d1dce424a459b82
Albert Wang <albert@cellabus.com>
2015-04-23 20:20:15 -0700

    Add AppleDEPClient::Account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+
+describe AppleDEPClient::Account do
+  it "can fetch data" do
+    expect(AppleDEPClient::Request).to receive(:make_request).with(AppleDEPClient::Account::URL, :get, '').and_return('asdf').once
+    expect(AppleDEPClient::Account.fetch).to eq 'asdf'
+  end
+end
