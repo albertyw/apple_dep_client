d470f77d070cc0db21744a17e76dab1691c7396b
Albert Wang <albert@cellabus.com>
2015-04-25 19:59:23 -0700

    Add unimplemented functions to Profile

--- lib/apple_dep_client/profile.rb
+++ lib/apple_dep_client/profile.rb
@@ -1,2 +1,23 @@
 # Methods for getting and setting device profiles
 
+require 'json'
+
+module AppleDEPClient
+  module Profile
+    def self.define(data)
+      raise NotImplementedError
+    end
+
+    def self.assign(profile_uuid, devices)
+      raise NotImplementedError
+    end
+
+    def self.fetch(profile_uuid)
+      raise NotImplementedError
+    end
+
+    def self.remove(devices)
+      raise NotImplementedError
+    end
+  end
+end
