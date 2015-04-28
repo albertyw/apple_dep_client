0a428f4034fbdd351e52320f5c4bae0ed8201590
Albert Wang <albert@cellabus.com>
2015-04-27 18:02:35 -0700

    Reset AppleDEPClient configuration for each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,16 @@ require 'simplecov'
 SimpleCov.start
 
 require 'apple_dep_client'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    AppleDEPClient.configure do |client|
+      client.private_key = nil
+      client.consumer_key = nil
+      client.consumer_secret = nil
+      client.access_token = nil
+      client.access_secret = nil
+      client.access_token_expiry = nil
+    end
+  end
+end
