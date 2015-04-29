91e915d075136c0559751b1af612ea17ef85d8c5
Albert Wang <albert@cellabus.com>
2015-04-29 00:31:00 -0700

    Simplify errors

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -2,155 +2,23 @@ require 'spec_helper'
 
 describe AppleDEPClient::Error do
   describe ".check_request_error" do
-    it "checks each error class" do
-      response = Typhoeus::Response.new
-      AppleDEPClient::Error.get_error_classes.each do |x|
-        expect(x).to receive(:check_response).with response
-      end
-      AppleDEPClient::Error.check_request_error response
+    it "raises an error if a lambda returns true" do
+      response = Typhoeus::Response.new(response_code: 400, body: '')
+      expect{AppleDEPClient::Error.check_request_error response}.to raise_error AppleDEPClient::Error::RequestError
     end
-    it "raises an error if a class returns true" do
-      response = Typhoeus::Response.new
-      expect(AppleDEPClient::Error::MalformedRequest).to receive(:check_response).and_return true
-      expect{AppleDEPClient::Error.check_request_error response}.to raise_error AppleDEPClient::Error::MalformedRequest
+    it "can also check for auth errors" do
+      response = Typhoeus::Response.new(response_code: 500, body: '')
+      expect{AppleDEPClient::Error.check_request_error(response, true)}.to raise_error AppleDEPClient::Error::RequestError
     end
   end
-  describe ".get_error_classes" do
-    it "returns all error classes" do
-      classes = AppleDEPClient::Error.get_error_classes
-      expect(classes.include? AppleDEPClient::Error::ExpiredCursor).to be_truthy
-      expect(classes.include? AppleDEPClient::Error::Auth::Forbidden).to be_falsey
-      expect(classes.include? AppleDEPClient::Error::GenericError).to be_truthy
+  describe ".get_errors" do
+    it "can return normal errors" do
+      errors = AppleDEPClient::Error.get_errors
+      expect(errors.map{|error| error[0]}).to include 'MalformedRequest'
     end
-    it "can also return auth classes" do
-      classes = AppleDEPClient::Error.get_error_classes(auth=true)
-      expect(classes.include? AppleDEPClient::Error::ExpiredCursor).to be_falsey
-      expect(classes.include? AppleDEPClient::Error::Auth::Forbidden).to be_truthy
-      expect(classes.include? AppleDEPClient::Error::GenericError).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::AbstractRequestError do
-    subject { AppleDEPClient::Error::AbstractRequestError }
-    let(:body) { 'asdf' }
-    it "takes a body" do
-      expect { raise subject.new(body) }.to raise_error RuntimeError
-    end
-    it "has no check_response function" do
-      expect{ subject.check_response 'x' }.to raise_error NotImplementedError
-    end
-  end
-  describe AppleDEPClient::Error::Auth do
-    describe AppleDEPClient::Error::Auth::BadRequest do
-      subject { AppleDEPClient::Error::Auth::BadRequest }
-      specify { expect(subject).to be }
-      it 'can return true for check response' do
-        response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: '')
-        expect(subject.check_response response).to be_truthy
-      end
-    end
-    describe AppleDEPClient::Error::Auth::Unauthorized do
-      subject { AppleDEPClient::Error::Auth::Unauthorized }
-      specify { expect(subject).to be }
-      it 'can return true for check response' do
-        response = Typhoeus::Response.new(return_code: :ok, response_code: 401, body: '')
-        expect(subject.check_response response).to be_truthy
-      end
-    end
-    describe AppleDEPClient::Error::Auth::Forbidden do
-      subject { AppleDEPClient::Error::Auth::Forbidden }
-      specify { expect(subject).to be }
-      it 'can return true for check response' do
-        response = Typhoeus::Response.new(return_code: :ok, response_code: 403)
-        expect(subject.check_response response).to be_truthy
-      end
-    end
-  end
-  describe AppleDEPClient::Error::GenericError do
-    subject { AppleDEPClient::Error::GenericError }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 500, body: '')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::MalformedRequest do
-    subject { AppleDEPClient::Error::MalformedRequest }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' MALFORMED_REQUEST_BODY ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::Unauthorized do
-    subject { AppleDEPClient::Error::Unauthorized }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 401, body: 'UNAUTHORIZED')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::Forbidden do
-    subject { AppleDEPClient::Error::Forbidden }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 403, body: 'FORBIDDEN')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::MethodNotAllowed do
-    subject { AppleDEPClient::Error::MethodNotAllowed }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 405, body: '')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::InvalidCursor do
-    subject { AppleDEPClient::Error::InvalidCursor }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' INVALID_CURSOR ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::ExhaustedCursor do
-    subject { AppleDEPClient::Error::ExhaustedCursor }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' EXHAUSTED_CURSOR ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::CursorRequired do
-    subject { AppleDEPClient::Error::CursorRequired }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' CURSOR_REQUIRED ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::ExpiredCursor do
-    subject { AppleDEPClient::Error::ExpiredCursor }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' EXPIRED_CURSOR ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::NotFound do
-    subject { AppleDEPClient::Error::NotFound }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 200, body: ' NOT_FOUND ')
-      expect(subject.check_response response).to be_truthy
-    end
-  end
-  describe AppleDEPClient::Error::DeviceIDRequired do
-    subject { AppleDEPClient::Error::DeviceIDRequired }
-    specify { expect(subject).to be }
-    it 'can return true for check response' do
-      response = Typhoeus::Response.new(return_code: :ok, response_code: 400, body: ' DEVICE_ID_REQUIRED ')
-      expect(subject.check_response response).to be_truthy
+    it "can return auth errors" do
+      errors = AppleDEPClient::Error.get_errors true
+      expect(errors.map{|error| error[0]}).to include 'BadRequest'
     end
   end
 end
