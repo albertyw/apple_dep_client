7f87ea6fbd599fd8e8eac9a3d04e947715f6be79
Albert Wang <albert@cellabus.com>
2015-04-30 19:50:51 -0700

    Add tests

---
+++ spec/callback_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe AppleDEPClient::Callback do
+  describe ".decode_callback" do
+    it "will call decrypt_data and feed data to parse_data" do
+      expect(AppleDEPClient::Callback).to receive(:decrypt_data).with('qwer').and_return('asdf').once
+      expect(AppleDEPClient::Callback).to receive(:parse_data).with('asdf').and_return('zxcv').once
+      AppleDEPClient::Callback.decode_callback 'qwer'
+    end
+  end
+
+  describe ".decrypt_data" do
+    it "will decrypt data and remove encryption data" do
+      expect(AppleDEPClient::Token).to receive(:run_command).and_return('data').once
+      expect(AppleDEPClient::Callback).to receive(:remove_encryption_data).with('data').once
+      AppleDEPClient::Callback.decrypt_data 'asdf'
+    end
+  end
+
+  describe ".remove_encryption_data" do
+    it "will only return data between doctype and plist" do
+      data = "asdfasdfasdf\n<!DOCTYPE>\ntext\ntext\n</plist>\nqwerqwerqwer"
+      cleaned_data = AppleDEPClient::Callback.remove_encryption_data data
+      expect(cleaned_data).to eq "<!DOCTYPE>\ntext\ntext\n</plist>"
+    end
+  end
+
+  describe ".parse_data" do
+    it "will return the parsed plist" do
+      data = "<plist><dict></dict></plist>"
+      data = AppleDEPClient::Callback.parse_data data
+      expect(data).to eq({})
+    end
+  end
+end
